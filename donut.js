695722abff4da06a890819201ee7051079fb6e6d
silvatim <silva.tim@gmail.com>
2017-01-25 16:24:32 +1100

    update json file change donut graph

--- donut.js
+++ donut.js
@@ -3,53 +3,67 @@ var renderDonut = function() {
 
   d3.json("data.json", function(error, data){
 
-  var dataset = [
-        { name: 'Salmon', count: 200 },
-        { name: 'Eggs', count: 890 },
-        { name: 'Apple', count: 360 },
-        { name: 'Nuts', count: 120 },
-        { name: 'Bread', count: 500 }
+  var vitaminA = [
+        { name: 'Salmon', amount: 200 },
+        { name: 'Eggs', amount: 890 },
+        { name: 'Apple', amount: 360 },
+        { name: 'Nuts', amount: 120 },
+        { name: 'Bread', amount: 500 }
     ];
 
     var total = 0;
 
-    dataset.forEach( function(d) {  total+= d.count;  });
+    //creating total amount for foods
+    vitaminA.forEach( function(food) {  total += food.amount;  });
+
 
     var donut = d3.layout.pie()
-             .value(function(d){ return d.count })
+             .value(function(food){ return food.amount })
              .sort(null);
 
     var width = 500;
     var height = 650;
-
+    console.log("run");
     var outerRadiusArc = width/2;
     var innerRadiusArc = 100;
     var shadowWidth = 20;
 
-    var outerRadiusArcShadow = innerRadiusArc+1;
-    var innerRadiusArcShadow = innerRadiusArc-shadowWidth;
+    var outerRadiusArcShadow = innerRadiusArc + 1;
+    var innerRadiusArcShadow = innerRadiusArc - shadowWidth;
 
-    var color = d3.scale.ordinal()
-     .range(['#41B787', '#6352B9', '#B65480', '#D5735A', '#D7D9DA']);
+    var color = d3.scale.ordinal().range(['#41B787', '#6352B9', '#B65480', '#D5735A', '#D7D9DA']);
 
     var svg = d3.select(".donutGraph")
             .append("svg")
-            .attr({ width: width, height: height, class:'shadow'})
+            // .attr("width", width)
+            // .attr("height", height);
+            // .attr({ width: width, height: height}) //class:'shadow'
             .attr("viewBox", "-250 -250 "+ width + " "+ height)
             .attr("preserveAspectRatio", "xMidYMid meet");
+            // .attr("preserveAspectRatio", "xMidYMid meet");
+             // .attr("transform", "translate(" + width + "," + height + ")");
+            // .append("g")
+            // .attr("viewBox", "0 0 "+ width/2 + " "+ height/2);
+
             // .attr({ transform:'translate('+ width / 2 +','+ height / 2 +')' })
+
 
     var createChart = function( svg, outerRadius, innerRadius, fillFunction, className ){
 
+        //draws arc
         var arc = d3.svg.arc()
                 .innerRadius(outerRadius)
                 .outerRadius(innerRadius);
 
+
         var path = svg.selectAll('.'+className)
-                .data( donut( dataset ) )
+                .data( donut( vitaminA ) )
                 .enter()
                 .append('path')
                 .attr({ class:className, d:arc, fill:fillFunction });
+
+               // path.append('text')
+               //     .text('hello');
 
         path.transition()
                 .duration(1000)
@@ -70,17 +84,17 @@ var renderDonut = function() {
         return d3.hsl((c.h+5), (c.s -.07), (c.l -.15));
     },'path2');
 
-    var addText = function (text,y,size) {
+    var addTextToCenter = function (text, y, size) {
         svg.append('text')
                 .text(text)
-                .attr({'text-anchor':'middle', y:y })
+                .attr({ 'text-anchor':'middle', y:y })
                 .style({ fill:'#929DAF', 'font-size':size });
     };
 
     var restOfTheData = function(){
 
-        addText(function(){ return "Intake Here"; },0,'30px');
-        addText( function(){ return "ug/mg";  },25,'10px');
+        addTextToCenter(function(){ return "400"; } ,0, '40px');
+        addTextToCenter( function(){ return "ug/mg";  } ,25, '30px');
 
     };
 
