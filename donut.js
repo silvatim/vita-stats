8d6638632b667b5fc88be28856a22cf357fed3c0
silvatim <silva.tim@gmail.com>
2017-01-26 18:59:43 +1100

    add vitamin intake list

--- donut.js
+++ donut.js
@@ -4,96 +4,96 @@ var renderDonut = function() {
   d3.json("data.json", function(error, data){
 
 var vitamins = {
-  "vitamin A": [
-    { name: 'Carrots', amount: 334 },
-    { name: 'Spinach', amount: 890 },
-    { name: 'Sweet Potato', amount: 360 },
+  "vitamin A": [ //mcg
+    { name: 'Carrots', amount: 852 },
+    { name: 'Spinach', amount: 500 },
+    { name: 'Sweet Potato', amount: 961 },
+    { name: 'Kale', amount: 550 },
+    { name: 'Tuna', amount: 757 }
+    ],
+  "vitamin B1": [ //mg
+    { name: 'Pork', amount: 1.1 },
+    { name: 'Bread', amount: 0.5 },
+    { name: 'Seeds', amount: 1.5 },
+    { name: 'Trout', amount: 0.4 },
+    { name: 'Beans', amount: 0.2 }
+    ],
+  "vitamin B2": [ //mg
+    { name: 'Almonds', amount: 1.1 },
+    { name: 'Egg', amount: 0.5},
+    { name: 'Beef', amount: 0.9 },
+    { name: 'Mushrooms', amount: 0.5 },
+    { name: 'Cheese', amount: 1.4 }
+    ],
+  "vitamin B3": [ //mg
+    { name: 'Chicken', amount: 15 },
+    { name: 'Peas', amount: 2 }
+    { name: 'Peanuts', amount: 14 },
+    { name: 'Beef', amount: 9 },
+    { name: 'Avocado', amount: 2 },
+    ],
+  "vitamin B5": [ //mg
+    { name: 'Cheese', amount: 3.3 },
+    { name: 'Pork', amount: 1.6 },
+    { name: 'Mushroom', amount: 3.6 },
+    { name: 'Trout', amount: 2.2 },
+    { name: 'Egg', amount: 1.5 }
+    ],
+  "vitamin B6": [//mcg
+    { name: 'Banana', amount: 0.4 },
+    { name: 'Turkey', amount: 0.8 },
+    { name: 'Garlic', amount: 1.2 },
+    { name: 'Salmon', amount: 0.6 },
+    { name: 'Chicken', amount: 0.5 }
+    ],
+  "vitamin B7": [ //mcg
+    { name: 'Egg', amount: 25 },
+    { name: 'Califlower', amount: 17 },
+    { name: 'Mushrooms', amount: 16 },
+    { name: 'Peas', amount: 18 },
+    { name: 'Peanuts', amount: 39 }
+    ],
+  "vitamin B9": [ //mcg
+    { name: 'Oranges', amount: 39 },
+    { name: 'Spinach', amount: 194 },
+    { name: 'Lentils', amount: 181 },
+    { name: 'Broccoli', amount: 108 },
+    { name: 'Beans', amount: 209 }
+    ],
+  "vitamin B12": [ //mcg
+    { name: 'Liver', amount: 70 },
+    { name: 'Cheese', amount: 3 },
+    { name: 'Cereals', amount: 20 },
+    { name: 'Shellfish', amount: 98},
+    { name: 'Egg', amount: 2 }
+    ],
+  "vitamin C": [ //mg
+    { name: 'Oranges', amount: 59},
     { name: 'Kale', amount: 120 },
-    { name: 'Tuna', amount: 500 }
+    { name: 'Kiwi', amount: 93 },
+    { name: 'Broccoli', amount: 89},
+    { name: 'Capsicum', amount: 128 }
     ],
-  "vitamin B1": [
-    { name: 'Pork', amount: 334 },
-    { name: 'Bread', amount: 890 },
-    { name: 'Seeds', amount: 360 },
-    { name: 'Trout', amount: 120 },
-    { name: 'Beans', amount: 500 }
+  "vitamin D": [ //mcg
+    { name: 'Salmon', amount: 19 },
+    { name: 'Egg', amount: 2 },
+    { name: 'Mushrooms', amount: 28 },
+    { name: 'Cereals', amount: 8 },
+    { name: 'Tofu', amount: 4 }
     ],
-  "vitamin B2": [
-    { name: 'Almonds', amount: 334 },
-    { name: 'Egg', amount: 890 },
-    { name: 'Beef', amount: 360 },
-    { name: 'Mushrooms', amount: 120 },
-    { name: 'Cheese', amount: 500 }
+  "vitamin E": [ //mg
+    { name: 'Almonds', amount: 26 },
+    { name: 'Spinach', amount: 2 },
+    { name: 'Kiwi', amount: 1.5 },
+    { name: 'Pumpkin', amount: 1 },
+    { name: 'Avocado', amount: 2 }
     ],
-  "vitamin B3": [
-    { name: 'Chicken', amount: 334 },
-    { name: 'Peanuts', amount: 890 },
-    { name: 'Beef', amount: 360 },
-    { name: 'Avocado', amount: 120 },
-    { name: 'Peas', amount: 500 }
-    ],
-  "vitamin B5": [
-    { name: 'Corn', amount: 334 },
-    { name: 'Broccoli', amount: 890 },
-    { name: 'Cauliflower', amount: 360 },
-    { name: 'Salmon', amount: 120 },
-    { name: 'Yoghurt', amount: 500 }
-    ],
-  "vitamin B6": [
-    { name: 'Banana', amount: 334 },
-    { name: 'Turkey', amount: 890 },
-    { name: 'Garlic', amount: 360 },
-    { name: 'Salmon', amount: 120 },
-    { name: 'Chicken', amount: 500 }
-    ],
-  "vitamin B7": [
-    { name: 'Peas', amount: 334 },
-    { name: 'Califlower', amount: 890 },
-    { name: 'Mushrooms', amount: 360 },
-    { name: 'Bananas', amount: 120 },
-    { name: 'Lentils', amount: 500 }
-    ],
-  "vitamin B9": [
-    { name: 'Oranges', amount: 334 },
-    { name: 'Spinach', amount: 890 },
-    { name: 'Lentils', amount: 360 },
-    { name: 'Broccoli', amount: 120 },
-    { name: 'Beans', amount: 500 }
-    ],
-  "vitamin B12": [
-    { name: 'Liver', amount: 600 },
-    { name: 'All Bran', amount: 290 },
-    { name: 'Shellfish', amount: 350 },
-    { name: 'Cheese', amount: 220 },
-    { name: 'Egg', amount: 530 }
-    ],
-  "vitamin C": [
-    { name: 'Oranges', amount: 334 },
-    { name: 'Kale', amount: 890 },
-    { name: 'Kiwi', amount: 360 },
-    { name: 'Broccoli', amount: 120 },
-    { name: 'Capsicum', amount: 500 }
-    ],
-  "vitamin D": [
-    { name: 'Salmon', amount: 334 },
-    { name: 'Tuna', amount: 890 },
-    { name: 'Mushrooms', amount: 360 },
-    { name: 'Egg', amount: 120 },
-    { name: 'Milk', amount: 500 }
-    ],
-  "vitamin E": [
-    { name: 'Almonds', amount: 334 },
-    { name: 'Spinach', amount: 890 },
-    { name: 'Wheat germ', amount: 360 },
-    { name: 'Pumpkin', amount: 120 },
-    { name: 'Avocado', amount: 500 }
-    ],
-  "vitamin K": [
-    { name: 'Sprouts', amount: 334 },
-    { name: 'Spinach', amount: 890 },
-    { name: 'Asparagus', amount: 360 },
-    { name: 'Kale', amount: 120 },
-    { name: 'Soy', amount: 500 }
+  "vitamin K": [ //mcg
+    { name: 'Sprouts', amount: 177 },
+    { name: 'Asparagus', amount: 41 },
+    { name: 'Spinach', amount: 483 },
+    { name: 'Kale', amount: 704 },
+    { name: 'Broccoli', amount: 102 }
     ]
 };
 var pie = d3.layout.pie()
@@ -105,8 +105,8 @@ var pie = d3.layout.pie()
 //  var h=300;
  var h=270;
 
-var outerRadius=w/2;
-var innerRadius=80;
+var outerRadius = w/2 ;
+var innerRadius = 80;
 
 var color = d3.scale.category10();
 
@@ -121,7 +121,7 @@ var svg = d3.select(".donutGraph")
   .attr({transform:'translate('+w/1.6+','+h/2+')'});
 
 var path = svg.selectAll('path')
-  .data(pie(vitamins["vitamin B12"]))
+  .data(pie(vitamins["vitamin C"]))
   .enter()
   .append('path')
   .attr({ d:arc, fill:function(d,i){ return color(d.data.name); } });
@@ -138,7 +138,7 @@ path.transition()
 
 var restOfTheData=function(){
     var text = svg.selectAll('text')
-        .data(pie(vitamins["vitamin B12"]))
+        .data(pie(vitamins["vitamin C"]))
         .enter()
         .append("text")
         .transition()
@@ -179,122 +179,8 @@ var restOfTheData=function(){
 setTimeout(restOfTheData,1000);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // var vitaminA = [
-  //       { name: 'Salmon', amount: 200 },
-  //       { name: 'Eggs', amount: 890 },
-  //       { name: 'Apple', amount: 360 },
-  //       { name: 'Nuts', amount: 120 },
-  //       { name: 'Bread', amount: 500 }
-  //   ];
-  //
-  //   var total = 0;
-  //
-  //   vitaminA.forEach( function(food) {  total += food.amount;  });
-  //
-  //   var pie = d3.layout.pie()
-  //            .value(function(food){ return food.amount })
-  //            .sort(null);
-  //
-  //   var width = 500;
-  //   var height = 650;
-  //   var outerRadiusArc = width/2;
-  //   var innerRadiusArc = 100;
-  //   var shadowWidth = 20;
-  //
-  //   var outerRadiusArcShadow = innerRadiusArc + 1;
-  //   var innerRadiusArcShadow = innerRadiusArc - shadowWidth;
-  //
   //   var color = d3.scale.ordinal().range(['#41B787', '#6352B9', '#B65480', '#D5735A', '#D7D9DA']);
-  //
-  //   var svg = d3.select(".donutGraph")
-  //           .append("svg")
-  //           // .attr({  width:width,  height:height,  class:'shadow' })
-  //          .attr("viewBox", "-250 -250 "+ width + " "+ height)
-  //           .attr("preserveAspectRatio", "xMidYMid meet");
-  //
-  //
-  //   var createChart = function( svg, outerRadius, innerRadius, fillFunction, className ){
-  //
-  //       //draws arc
-  //       var arc = d3.svg.arc()
-  //               .innerRadius(outerRadius)
-  //               .outerRadius(innerRadius);
-  //
-  //
-  //       var path = svg.selectAll('.'+className)
-  //               .data( pie( vitaminA ) )
-  //               .enter()
-  //               .append('path')
-  //               .attr({ class:className, d:arc, fill:fillFunction });
-  //
-  //
-  //       path.transition()
-  //               .duration(1000)
-  //               .attrTween('d', function(d) {
-  //                   var interpolate = d3.interpolate({startAngle: 0, endAngle: 0}, d);
-  //                   return function(t) { return arc(interpolate(t)); };
-  //               });
-  //   };
-  //
-  //
-  //   createChart(svg,outerRadiusArc,innerRadiusArc,function(d,i){
-  //       return color(d.data.name);
-  //   },'path1');
-  //
-  //   createChart(svg,outerRadiusArcShadow,innerRadiusArcShadow,function(d,i){
-  //       var c = d3.hsl(color(d.data.name));
-  //       return d3.hsl((c.h+5), (c.s -.07), (c.l -.15));
-  //   },'path2');
-  //
-  //
-  //   var addTextToCenter = function (text, y, size) {
-  //       svg.append('text')
-  //               .text(text)
-  //               .attr({ 'text-anchor':'middle', y:y })
-  //               .style({ fill:'#929DAF', 'font-size':size });
-  //   };
-  //
-  //   var restOfTheData = function(){
-  //       addTextToCenter(function(){ return "400"; } ,0, '40px');
-  //       addTextToCenter(function(){ return "ug/mg";  } ,25, '30px');
-  //   };
-  //
-  //   setTimeout(restOfTheData,1000);
-  //   });
-  //
+
  });
 
 };
